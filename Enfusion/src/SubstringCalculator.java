d18043a1bdbea056a9d3d26f6016e7f9e9e888a8
Alberto Pizano <apizano@purdue.edu>
2020-03-06 15:06:38 -0600

    starting base logic for substring calcuator

--- Enfusion/src/SubstringCalculator.java
+++ Enfusion/src/SubstringCalculator.java
@@ -10,6 +10,16 @@ public class SubstringCalculator {
     public static void main(String[] args) {
         String s = "abcde";
 
+        int ptr1 = 0;
+        int uniqueCount = 0;
+        String memory = "";
+
+        for(int i=0; i<s.length(); i++) {
+            if (!memory.contains(Character.toString(s.charAt(ptr1)))) {
+                uniqueCount++;
+            }
+        }
+
         System.out.println("hello world");
 
 
