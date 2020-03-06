d8720a12945837ffed9583b92b680f1d656b1ae5
Alberto Pizano <apizano@purdue.edu>
2020-03-06 13:22:30 -0600

    adding constraints to the problem

--- Enfusion/src/SubstringCalculator.java
+++ Enfusion/src/SubstringCalculator.java
@@ -1,5 +1,17 @@
 public class SubstringCalculator {
+
+    public static int substringCalculator(String s ) {
+        return 0;
+    }
+
+    // constrains
+    // String s consists of chars in the range ascii[a-z]
+    // 0 <= |s| <= 10^5
     public static void main(String[] args) {
+        String s = "abcde";
+
         System.out.println("hello world");
+
+
     }
 }
