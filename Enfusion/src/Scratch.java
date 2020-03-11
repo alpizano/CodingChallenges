4a6deb322d00d92646f1fbef887df95b80aeba6b
Alberto Pizano <apizano@purdue.edu>
2020-03-11 17:09:22 -0500

    testing more

--- Enfusion/src/Scratch.java
+++ Enfusion/src/Scratch.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Hashtable;
 import java.util.List;
 
@@ -9,6 +10,7 @@ public class Scratch {
         List<String> strList = new ArrayList<>();
         List<String> strList2 = new ArrayList<>();
         List<String> control = new ArrayList<>();
+        List<String> testSort = new ArrayList<>();
 
         if(test.contains("h"))  {
             System.out.println("Yes it does");
@@ -26,19 +28,39 @@ public class Scratch {
         control.add("A");
         control.add("B");
 
+        testSort.add("C");
+        testSort.add("B");
+        testSort.add("A");
+
         System.out.println(strList);
         System.out.println(strList.equals(strList2));
 
         System.out.println(strList.containsAll(strList2));
 
         table.put(strList,1);
-        table.put(control, 1);
+        //table.put(control, 1);
        // table.put(strList2,1);
 
         System.out.println("Checking if table contains list (irrelevant order):");
+        System.out.println("If table with List ('A','B') contains ('B','A') should be false");
         System.out.println(table.contains(strList2));
+        System.out.println("If table with List ('A','B') contains different object ('A','B') should be false");
         System.out.println(table.contains(control));
+        System.out.println("If table with List ('A','B') contains same object ('A','B') should be true");
+        System.out.println(table.contains(strList));
+        System.out.println("If table contains the same List with the same contents (Different object)");
         System.out.println(table.containsKey(strList));
+        System.out.println("If table contains a different List with the same contents (Different object)");
+        System.out.println(table.containsKey(control));
+        System.out.println("If table contains a different List with different contents (Different object)");
+        System.out.println(table.containsKey(strList2));
+        //System.out.println(table.containsKey(new ArrayList<String>("A","B")));
+
+
+        System.out.println(testSort);
+        Collections.sort(testSort);
+
+        System.out.println(testSort);
 
     }
 }
--- Enfusion/src/SubstringCalculator.java
+++ Enfusion/src/SubstringCalculator.java
@@ -1,20 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Hashtable;
+import java.util.List;
 
 public class SubstringCalculator {
 
     public static int substringCalculator(String s ) {
 
-        Hashtable<String, Integer> table = new Hashtable<>();
+        Hashtable<List<String>, Integer> table = new Hashtable<>();
+        List<String> list = new ArrayList<String>();
 
         int ptr1 = 0;
         int uniqueCount = 0;
         String memory = "";
 
+        // String s =  "abcdef"
+        // iterate through a, then ab , abc
+
         for(int i=0; i<s.length(); i++) {
+            // add char to List, so becomes ("A"), then ("A","B")
+            list.add(Character.toString(s.charAt(i)));
+            // sort before inserting into hash table
+            Collections.sort(list);
+            table.put(list, 1);
 
-            if (!table.contains(Character.toString(s.charAt(i)))) {
+            for(int k=1; k<s.length(); k++) {
+                if (!table.contains(Character.toString(s.charAt(i)))) {
 
-            table.put(Character.toString(s.charAt(ptr1)),1);
+                    //table.put(Character.toString(s.charAt(ptr1)),1);
+            }
+
+
+
 
             }
         }
