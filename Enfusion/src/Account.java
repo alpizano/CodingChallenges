9b84ece24e051c3856d90f525b5de328410eb4f9
Alberto Pizano <apizano@purdue.edu>
2020-03-21 20:25:17 -0500

    finished writing Account class

--- Enfusion/src/Account.java
+++ Enfusion/src/Account.java
@@ -1,2 +1,29 @@
 public class Account {
+    int balance;
+
+    public Account() {
+        balance = 0;
+    }
+
+    public String deposit(int money) {
+        return "Despositing " + money;
+    }
+
+    public String withdraw(int money) {
+        int newBalance = money - balance;
+
+        if(newBalance > 0) {
+            balance = balance - money;
+            return "Withdrawing " + money;
+
+        }
+        else {
+            return "Withdrawing " + money + " (Insufficient Balance)";
+        }
+    }
+
+
+    public int getBalance() {
+        return balance;
+    }
 }
