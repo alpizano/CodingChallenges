c641c7d3d1d6485c973694664baf0048fc0f9e9f
Alberto Pizano <apizano@purdue.edu>
2020-03-21 20:38:36 -0500

    finished writing Transaction class

--- Enfusion/src/Transaction.java
+++ Enfusion/src/Transaction.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Transaction {
+    private Account account;
+    private List<String> transactions;
+
+    public Transaction() {
+        account = new Account();
+        transactions = new ArrayList<>();
+    }
+
+    void deposit(int money) {
+        transactions.add(account.deposit(money));
+    }
+
+    void withdraw(int money) {
+        transactions.add(account.deposit(money));
+    }
+
+    List<String> getTransaction() {
+        return transactions;
+    }
 }
