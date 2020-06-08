bc2c937c50d135de817bcd60443850c1bacd0014
alpizano <apizano@purdue.edu>
2020-06-08 01:42:47 -0500

    Adding definition of LinkedList class

--- ReverseLinkedList/src/main/java/org/example/Solution.java
+++ ReverseLinkedList/src/main/java/org/example/Solution.java
@@ -1,11 +1,21 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+
 public class Solution
 {
+    public static LinkedList reverseLinkedList(LinkedList head) {
+        return null;
+    }
+
+    static class LinkedList {
+        int value;
+        LinkedList next;
+
+        public LinkedList(int value) {
+            this.value=value;
+            next = null;
+        }
+    }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
