82c4a506b2c6c477f1eb2c2cf7d71dc9bc709b65
alpizano <apizano@purdue.edu>
2020-10-27 08:02:23 -0500

    finished binary tree pruning

--- CapitalOne/src/BinaryTreePruning.java
+++ CapitalOne/src/BinaryTreePruning.java
@@ -1,43 +1,77 @@
 public class BinaryTreePruning {
+    //boolean containsOne = false;
 
-//    public TreeNode pruneTree(TreeNode root) {
-public void printTree(TreeNode root) {
-    // 1. Figure out subtree traversal
-    // 2. Figure out how to delete nodes
+    public TreeNode pruneTree2(TreeNode root) {
+        if (root == null) return null;
 
-     //  Preorder Traversal
-        System.out.println(root.val);
-        if( root.left != null) {
-            printTree(root.left);
+        root.left = pruneTree2(root.left);
+        root.right = pruneTree2(root.right);
+        if (root.val == 0 && root.left == null && root.right == null) {
+            root = null;
         }
-       if(root.right != null) {
-            printTree(root.right);
+
+        return root;
+    }
+
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) {
+            return null;
         }
+        containsOne(root);
+        return root;
+    }
+
+    public boolean containsOne(TreeNode root) {
+        if (root == null) return false;
+
+        boolean left_contains = containsOne(root.left);
+        boolean right_contains = containsOne(root.right);
+
+        if (!left_contains) {
+            root.left = null;
+        }
+        if (!right_contains) {
+            root.right = null;
+        }
+
+        return root.val == 1 || left_contains || right_contains;
+
     }
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1, null, new TreeNode(0, new TreeNode(0), new TreeNode(1)));
+        TreeNode root3 = new TreeNode(1, new TreeNode(0, new TreeNode(0), new TreeNode(0)), new TreeNode(1, new TreeNode(0), new TreeNode(1)));
 
         TreeNode root2 = new TreeNode(14,
                 new TreeNode(17, new TreeNode(9, null, new TreeNode(13)), new TreeNode(53)),
                 new TreeNode(11));
 
-        BinaryTreePruning test  = new BinaryTreePruning();
-        test.printTree(root2);
+        BinaryTreePruning test = new BinaryTreePruning();
+
+//        test.pruneTree(root2);
+        test.pruneTree2(root);
+//        test.pruneTree(root3);
+
     }
 }
 
 
-  class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-         this.val = val;
-         this.left = left;
-          this.right = right;
-      }
-  }
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 
