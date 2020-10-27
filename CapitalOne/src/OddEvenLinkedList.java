9c51f20e8f42f2b6e566093b281857ee5011d2a7
alpizano <apizano@purdue.edu>
2020-10-27 08:04:09 -0500

    adding odd even linked list

---
+++ CapitalOne/src/OddEvenLinkedList.java
@@ -0,0 +1,27 @@
+public class OddEvenLinkedList {
+    public ListNode oddEvenList(ListNode head) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
