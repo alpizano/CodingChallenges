628d5a2e4820c3868afdbf12a9bf4ac7f8fbf5da
kbrv37 <apizano@purdue.edu>
2020-10-28 03:09:00 -0500

    adding pretty print for linked list

--- CapitalOne/src/OddEvenLinkedList.java
+++ CapitalOne/src/OddEvenLinkedList.java
@@ -3,8 +3,21 @@ public class OddEvenLinkedList {
         return null;
     }
 
-    public static void main(String[] args) {
+    public void prettyPrint(ListNode head) {
+        for (ListNode cursor = head; cursor != null; cursor = cursor.next) {
+            System.out.print(cursor.val);
+            System.out.print("->");
 
+            if (cursor.next == null) {
+                System.out.print("NULL");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        OddEvenLinkedList test = new OddEvenLinkedList();
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        test.prettyPrint(list1);
     }
 
 }
