39f6fc7de7d89d09101b459542d658cec2df95fd
kbrv37 <apizano@purdue.edu>
2020-11-01 06:09:27 -0600

    adding interface for time

--- AffinipayAssessment/src/main/java/com/affinipay/ITime.java
+++ AffinipayAssessment/src/main/java/com/affinipay/ITime.java
@@ -1,5 +1,7 @@
 package com.affinipay;
 
+import com.affinipay.exceptions.InvalidTwelveHourTimeFormatException;
+
 public interface ITime {
-    String addMinutes(String time, int minutesToAdd);
+    String addMinutes(String time, int minutesToAdd) throws InvalidTwelveHourTimeFormatException;
 }
