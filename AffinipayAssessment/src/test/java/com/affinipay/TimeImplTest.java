083f729c2724fdc864018dde7e6fca24e44d5ac8
kbrv37 <apizano@purdue.edu>
2020-11-01 11:18:46 -0600

    remove unused imports

--- AffinipayAssessment/src/test/java/com/affinipay/TimeImplTest.java
+++ AffinipayAssessment/src/test/java/com/affinipay/TimeImplTest.java
@@ -1,8 +1,6 @@
 package com.affinipay;
 
 import com.affinipay.exceptions.InvalidTwelveHourTimeFormatException;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
