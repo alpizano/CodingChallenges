f95571bdbac79315038fd759a43bf4383f56b217
kbrv37 <apizano@purdue.edu>
2020-11-03 04:47:50 -0600

    solving symmetric tree using arraylist

--- CapitalOne/src/SymmetricTree.java
+++ CapitalOne/src/SymmetricTree.java
@@ -1,2 +1,47 @@
+import java.util.*;
+
 public class SymmetricTree {
+    List<String> treeList = new ArrayList();
+
+    public boolean isSymmetric(TreeNode root) {
+        boolean isSymmetric = true;
+        int depth = 0;
+        populateArray(root, depth);
+
+        System.out.println(treeList);
+
+//        int[] intArray = treeList.stream().mapToInt(i -> i).toArray();
+        String[] strArray = treeList.toArray(new String[treeList.size()]);
+
+        for (int i = 0, j = strArray.length - 1; i < strArray.length / 2; i++, j--) {
+            if (!strArray[i].equals(strArray[j])) {
+                isSymmetric = false;
+            }
+        }
+
+        return isSymmetric;
+    }
+
+    public void populateArray(TreeNode root, int depth) {
+        if (root == null) {
+            treeList.add(depth + "-" + "null");
+            return;
+        }
+
+        populateArray(root.left, depth + 1);
+        treeList.add(depth + "-" + root.val);
+        populateArray(root.right, depth + 1);
+    }
+
+    public static void main(String[] args) {
+        SymmetricTree test = new SymmetricTree();
+
+        TreeNode root = new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+                new TreeNode(2, new TreeNode(4), new TreeNode(3)));
+        test.isSymmetric(root);
+
+
+    }
 }
+
+
