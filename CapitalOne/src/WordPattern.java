c52200f40ef522e6acdb207950736b3a018c29f5
kbrv37 <apizano@purdue.edu>
2020-11-05 11:54:35 -0600

    starting word pattern problem

---
+++ CapitalOne/src/WordPattern.java
@@ -0,0 +1,10 @@
+public class WordPattern {
+    public boolean wordPattern(String pattern, String s) {
+
+        return false;
+    }
+    public static void main(String[] args) {
+        WordPattern test = new WordPattern();
+        test.wordPattern("abba", "dog cat cat dog");
+    }
+}
