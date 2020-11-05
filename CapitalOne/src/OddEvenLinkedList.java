71ec1bc6df0f6b42bd05b471e26a7742680ad3be
kbrv37 <apizano@purdue.edu>
2020-11-05 11:51:25 -0600

    odd even linked list final implementation

--- CapitalOne/src/OddEvenLinkedList.java
+++ CapitalOne/src/OddEvenLinkedList.java
@@ -52,20 +52,3 @@ public class OddEvenLinkedList {
     }
 
 }
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
