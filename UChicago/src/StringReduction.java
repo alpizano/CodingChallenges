43f05baafe88cc332ac19e89bbfbbb17c0084ae8
kbrv37 <apizano@purdue.edu>
2020-11-06 07:41:27 -0600

    adding string reduction problem

--- UChicago/src/StringReduction.java
+++ UChicago/src/StringReduction.java
@@ -1,25 +1,96 @@
+import java.util.*;
+
 public class StringReduction {
 
     public int stringReduction(String s) {
-        init(new char[26]);
+        StringBuilder sb = new StringBuilder(s);
+        Map<Character,String> referenceMap = new LinkedHashMap<>();
+        Set<Character> set = new HashSet<>();
+        referenceMap.put('a',"");
+        referenceMap.put('b',"");
+        referenceMap.put('c',"");
 
-        return 0;
-    }
-
-    public void init(char[] charArray) {
-        char letter = 'a';
-
-        for(int i=0; i<charArray.length; i++) {
-            charArray[i] = letter++;
-            //System.out.print(charArray[i] + " ");
+        if(s.length() == 0 | s.length() == 1) {
+            return s.length();
         }
+        int threshold = 0;
+        int i=0;
+
+        while(i < sb.length()) {
+        //for (int i = 0; i < sb.length(); i++) {
+
+            if(!set.contains(sb.charAt(i))) {
+                set.add(sb.charAt(i));
+
+            }
+           else{
+//               set.clear();
+                threshold++;
+
+            }
+            // we got 2 unique values
+            if (set.size() == 2) {
+                Map<Character,String> temp = new HashMap<>(referenceMap);
+                temp.keySet().removeAll(set);
+                //System.out.println(temp);
+
+
+                while (i >= threshold) {
+                    sb.deleteCharAt(i);
+                    i--;
+                }
+
+               sb.insert(threshold,temp.keySet().toArray()[0]);
+                //threshold++;
+               set.clear();
+                i = 0;
+                threshold = 0;
+                //break;
+                continue;
+            }
+
+                i++;
+        }
+       //System.out.println(sb.toString());
+
+        return sb.length();
     }
+
 
     public static void main(String[] args) {
         StringReduction test = new StringReduction();
-        test.stringReduction("cab");
-        test.stringReduction("bcab");
-        test.stringReduction("ccccc");
+
+        System.out.println(test.stringReduction("aba")); // aba -> ca -> b
+        // Test case 0
+        System.out.println(test.stringReduction("cab")); // cab -> cc or cab -> bb
+        System.out.println( test.stringReduction("bcab")); // bcab -> aab -> ac -> b
+        System.out.println(test.stringReduction("ccccc"));
+        System.out.println("----------------------");
+        // Test case 1, 2 - 10
+        System.out.println("1. "+ test.stringReduction("babcbbaabcbcbcbaabbccaacccbbbcaaacabbbbaaaccbcccacbbccaccbbaacaccbabcaaaacaccacbaacc"));
+        System.out.println("2. "+ test.stringReduction("accbaacabbbaacabcbcaccaabcbccbacbcbaabacacababcaacbcccbaccacaabcbaaccbcabccbccbcbbcba"));
+        System.out.println("3. "+ test.stringReduction( "ccaacbabbccbcca"));
+        System.out.println("4. "+ test.stringReduction("cbcbabccaaaacbbbcccbbb"));
+        System.out.println("5. "+ test.stringReduction("aaacacaacccbcbacccbacacbaabcc"));
+        System.out.println("6. "+ test.stringReduction("acabcbbcbabbabcacbaaccb"));
+        System.out.println("7. "+ test.stringReduction("ccaacaaccabccacabbcbabccbaabacbcccabcaaabaabaccbbaaccacaababbcbacbbcccaccababbccbac"));
+        System.out.println("8. "+ test.stringReduction("accbacbbcccccaccaababcbccacabbbcbbbcbcbbcaabbbccbaaabbaaaaabcabbcaabacacaaccbbbaccbaacbbcaaba"));
+        System.out.println("9. "+ test.stringReduction("aaccbccbccabbcc"));
+        System.out.println("10. "+ test.stringReduction("acccabcccbcaaacbacbcbabbccacb"));
+        System.out.println("----------------------");
+        // Test case 1, 11 - 20
+        System.out.println("11. " + test.stringReduction("ccaabacaacccaaaacaaaacacaabbcccaccbbaaabcababcbaacbbbcbabccaacbaccccaabbbbbcaabbcabcacabcccab"));
+        System.out.println("12. " +test.stringReduction("aabaaabc"));
+        System.out.println("13. " +test.stringReduction("caacbbaabbbbcbbbbbabaccbcaccbcbbabccacc"));
+        System.out.println("14. " +test.stringReduction("bbbbaacbcbaaaabaabcaaacbab"));
+        System.out.println("15. " +test.stringReduction("acbcbacaacaccbacabbacabcbccacbbaacacaccaccaccacccbabcbcabcabaccbcaacaaabaabcbacacacbcbcbaaabaccaaaac"));
+        System.out.println("16. " +test.stringReduction("baccbcabacbbccaabcbaacaaabbacacbbcaacbbcbaaaacaabcabcbbccaacbaaaabaaacacaccabbbaabcacbacabcbbcccb"));
+        System.out.println("17. " +test.stringReduction("abcbaaaaccbaccccacbabbbaabaabbbcabababbcbaacaabbaabbcbaaabbbaaabcacacccbaaab"));
+        System.out.println("18. " +test.stringReduction("ccbacacabaaaacbaacaacabcaaabbbcacabbbcbaccaacbbcbccbabbabcccbbbaccbbbaccacccbbbabcccaacaa"));
+        System.out.println("19. " + test.stringReduction("acbbccccbacacbcaabbcbccbcccaaaabccaabcccbccbacbcaacc"));
+        System.out.println("20. " + test.stringReduction("bbabbaccbbcabaccccaccbbcbabbabcbaaccbbcc"));
+
+
 
 
     }
