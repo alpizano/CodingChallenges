84cde56d51698e9040f36876ef2aa35219644cd6
kbrv37 <apizano@purdue.edu>
2020-11-08 11:59:38 -0600

    Implementing copy list with random pointer

--- CapitalOne/src/CopyListWithRandomPointer.java
+++ CapitalOne/src/CopyListWithRandomPointer.java
@@ -1,9 +1,51 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class CopyListWithRandomPointer {
     public Node copyRandomList(Node head) {
+        // Key = val, Value = index
+        Map<Integer,Integer> map = new HashMap<>();
+        Node newHead;
 
+        int index =0;
+        for(Node cursor=head; cursor != null; cursor = cursor.next) {
+            map.put(cursor.val, index++);
+        }
 
-        return null;
+        System.out.println(map);
+
+        for(Node cursor=head; cursor != null; cursor = cursor.next) {
+            //newHead = new Node(cursor.val);
+            addToTail(head, cursor.val);
+        }
+
+        printList(head);
+
+        return head;
     }
+
+    public void addToTail(Node head, int val) {
+        Node newNode = new Node(val);
+        Node cursor = head;
+
+        while(cursor != null) {
+            if(cursor.next == null) {
+                //newNode = cursor;
+                cursor.next = newNode;
+            }
+        }
+    }
+
+    public void printList(Node head) {
+
+        for(Node cursor=head; cursor != null; cursor = cursor.next) {
+            System.out.print(cursor.val);
+            if(cursor.next != null) {
+                System.out.print("->");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Node head = new Node(7);
         head.next = new Node(13);
