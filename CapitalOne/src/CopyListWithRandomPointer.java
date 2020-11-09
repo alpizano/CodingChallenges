f383d1fdbad2af14dc13c9a0ce9a6a6677f3dccd
kbrv37 <apizano@purdue.edu>
2020-11-08 18:13:10 -0600

    finished solution to copy list with random pointer

--- CapitalOne/src/CopyListWithRandomPointer.java
+++ CapitalOne/src/CopyListWithRandomPointer.java
@@ -1,63 +1,83 @@
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class CopyListWithRandomPointer {
     public Node copyRandomList(Node head) {
-        // Key = val, Value = index
-        Map<Integer,Integer> map = new HashMap<>();
-        Node newHead;
+        Map<Node,Integer> keyNodeValueIndex = new LinkedHashMap<>();
+        Map<Integer,Node> keyIndexValueRandomPointerNode = new LinkedHashMap<>();
+        Map<Integer,Node> keyIndexValueReferenceToNode = new LinkedHashMap<>();
+        Node newHead = null;
 
         int index =0;
         for(Node cursor=head; cursor != null; cursor = cursor.next) {
-            map.put(cursor.val, index++);
+            keyNodeValueIndex.put(cursor, index);
+            keyIndexValueRandomPointerNode.put(index, cursor.random == null ? null : cursor.random);
+            newHead = addToTail(cursor, newHead, keyIndexValueReferenceToNode, index);
+            index++;
         }
 
-        System.out.println(map);
-
-        for(Node cursor=head; cursor != null; cursor = cursor.next) {
-            //newHead = new Node(cursor.val);
-            addToTail(head, cursor.val);
+        index = 0;
+        for(Node cursor=newHead; cursor != null; cursor = cursor.next) {
+            cursor.random = keyIndexValueRandomPointerNode.get(index) == null ? null : keyIndexValueReferenceToNode.get(keyNodeValueIndex.get(keyIndexValueRandomPointerNode.get(index)));
+            index++;
         }
 
-        printList(head);
-
-        return head;
+        return newHead;
     }
 
-    public void addToTail(Node head, int val) {
-        Node newNode = new Node(val);
-        Node cursor = head;
+    public Node addToTail(Node currNode, Node newHead, Map<Integer,Node> map, int index) {
+        Node newNode = new Node(currNode.val);
+        map.put(index, newNode);
+
+        Node cursor = newHead;
+
+        if(newHead == null) {
+            newHead = newNode;
+            return newHead;
+        }
 
         while(cursor != null) {
             if(cursor.next == null) {
-                //newNode = cursor;
                 cursor.next = newNode;
+                break;
             }
+            cursor = cursor.next;
         }
+
+        return newHead;
     }
+
 
     public void printList(Node head) {
 
         for(Node cursor=head; cursor != null; cursor = cursor.next) {
-            System.out.print(cursor.val);
+            System.out.print("["+cursor+","+cursor.val+","+cursor.random+","+cursor.next+"]");
             if(cursor.next != null) {
                 System.out.print("->");
             }
         }
+        System.out.println();
     }
 
     public static void main(String[] args) {
+        // Node 7
         Node head = new Node(7);
-        head.next = new Node(13);
         head.random = null;
-        head.next.next = new Node(11);
+        // node 13
+        head.next = new Node(13);
         head.next.random =  head;
-        head.next.next.next = new Node(11);
-        head.next.next.next.next = new Node(10);
-        head.next.next.next.next.random = head.next.next.next.next.next;
-        head.next.next.next.next.next = new Node(1);
-        head.next.next.next.next.next.random = head;
-        head.next.next.next.random = head.next.next.next.next.next;
+        // node 11
+        head.next.next = new Node(11);
+        // node 10
+        head.next.next.next = new Node(10);
+        head.next.next.next.random = head.next.next;
+        // node 1
+        head.next.next.next.next = new Node(1);
+        head.next.next.next.next.random = head;
+        // node 11
+        head.next.next.random = head.next.next.next.next;
+
 
 
         CopyListWithRandomPointer test = new CopyListWithRandomPointer();
