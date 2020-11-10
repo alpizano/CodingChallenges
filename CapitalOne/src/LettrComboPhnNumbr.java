665c4198ce8ada7a0406d4f44f8604dd47118da0
kbrv37 <apizano@purdue.edu>
2020-11-09 20:35:41 -0600

    starting letter combinations of a phone number problem

---
+++ CapitalOne/src/LettrComboPhnNumbr.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LettrComboPhnNumbr {
+    public List<String> letterCombinations(String digits) {
+        List<String> strList = new ArrayList<>();
+
+        return strList;
+    }
+
+    public static void main(String[] args) {
+        LettrComboPhnNumbr test = new LettrComboPhnNumbr();
+        test.letterCombinations("");
+    }
+}
